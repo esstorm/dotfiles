f6ee93f4b06f9f572f994efe2afaefd21bb66cee
torres <e.torres.menendez@gmail.com>
2020-02-14 14:24:55 +0100

    Adding i3wm and polybar configs

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+#while pgrep -u $UID -x polybar >/dev/null; do
+   #sleep 1;
+#done
+
+# The main bar includes extra modules to see the time, weather, etc
+for m in $(polybar --list-monitors | grep "primary" | cut -d":" -f1); do
+   echo $m
+    MONITOR=$m polybar --reload main -c ~/.config/polybar/config.ini &
+done
+
+for m in $(polybar --list-monitors | grep -v "primary" | cut -d":" -f1); do
+    MONITOR=$m polybar --reload secondary -c ~/.config/polybar/config.ini &
+done
+
